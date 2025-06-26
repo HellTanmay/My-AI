9651d15b37b95301dd0e0d1fc46f287fa369bb4d
HellTanmay <ailtanmay12@gmail.com>
2025-06-26 17:17:23 +0530

    Allowed headers

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const generate=require("./AiConfig")
 const app = express()
-app.use(cors({allowedHeaders:"https://vercel.com/ailtanmay12-gmailcoms-projects/my-ai-cso6/GEHq8Ur8Vahm7JvyCw9GqVacXBaw"}))
+app.use(cors( {origin: ["http://localhost:3000", "https://my-ai-cso6.vercel.app"]}))
 
 const port = 3000
 
